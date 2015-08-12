eee8cb9b5d97252943624092bd003d6b4dc7db45
danielrbetteridge@gmail.com <danielrbetteridge@gmail.com>
2015-08-12 21:13:04 +0800

    javascript page reload

--- planit/urls.py
+++ planit/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from flights.views import flights_main, flights_refresh
+from flights.views import flights_main
 from django.conf import settings
 from django.conf.urls.static import static
 from flights.views import FlightListView
@@ -13,5 +13,5 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', flights_main, name='main'),
     url(r'^flights/$', FlightListView.as_view(), name='flights-detail' ),
-    url(r'^flights/refresh/$', flights_refresh, name='refresh')
+    
 ) 
