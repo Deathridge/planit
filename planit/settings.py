62ea55732578277a1e6cee53e726be55be75e324
danielrbetteridge@gmail.com <danielrbetteridge@gmail.com>
2015-08-12 18:06:47 +0800

    fix for templatetags issue

--- planit/settings.py
+++ planit/settings.py
@@ -93,8 +93,7 @@ TEMPLATES = [
             ],
             'loaders': [
                 'django.template.loaders.filesystem.Loader',
-                'django.template.loaders.app_directories.Loader'
-                'django.template.loaders.app_directories.load_template_source',
+                'django.template.loaders.app_directories.Loader',
             ]
         },
     },
