9381c223bf0d6755333e1bded377adb4e2eb272c
danielrbetteridge@gmail.com <danielrbetteridge@gmail.com>
2015-08-12 18:01:22 +0800

    css changes for height of window

--- planit/settings.py
+++ planit/settings.py
@@ -94,6 +94,7 @@ TEMPLATES = [
             'loaders': [
                 'django.template.loaders.filesystem.Loader',
                 'django.template.loaders.app_directories.Loader'
+                'django.template.loaders.app_directories.load_template_source',
             ]
         },
     },
