791402abaa3b3d1f1039e49c995197f3a9bdad7b
danielrbetteridge@gmail.com <danielrbetteridge@gmail.com>
2015-08-12 19:46:52 +0800

    Move heading to navbar

--- static/js/timedrefresh.js
+++ static/js/timedrefresh.js
@@ -1,7 +1,7 @@
 // Refresh the Table every 5 seconds
 setInterval(function(){
     $.ajax({    
-       url: '{% url "flights-detail" %}',
+       url: {% url "flights-detail" %},
           success: function(data) {
           $('#flights').html(data);
           }
