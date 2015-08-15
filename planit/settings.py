3e4147d2891894fcea2fb40353bc96f87eadc9dc
Daniel Betteridge <danielrbetteridge@gmail.com>
2015-08-15 08:43:39 +0800

    setup postgres db

--- planit/settings.py
+++ planit/settings.py
@@ -66,12 +66,12 @@ WSGI_APPLICATION = 'planit.wsgi.application'
 import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 TEMPLATES = [
