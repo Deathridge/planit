00f8f1e2489a04fb94b31c115ac9768c02920651
Deathridge <danielrbetteridge@gmail.com>
2015-08-18 15:36:27 +0800

    no refresh form post

--- flights/models.py
+++ flights/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Flight(models.Model):	
-	FlightCode = models.CharField(blank=False, null=False, primary_key=True, max_length=255)
+	FlightID = models.AutoField(primary_key=True)
+	FlightCode = models.CharField(blank=True, null=True, max_length=255)
 	DepartureDate = models.DateField(blank=True,null=True)	
 	DepartureTime = models.TimeField(blank=True, null=True)
 	ArrivalTime = models.TimeField(blank=True, null=True)
