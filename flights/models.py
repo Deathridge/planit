a65fd48d142fe89021ad0ba6a52943d0b14b55fa
Deathridge <danielrbetteridge@gmail.com>
2015-08-18 16:30:42 +0800

    add pk default

--- flights/models.py
+++ flights/models.py
@@ -2,8 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Flight(models.Model):	
-	FlightID = models.AutoField(primary_key=True,default='1')
-	FlightCode = models.CharField(blank=True, null=True, max_length=255)
+	FlightCode = models.CharField(blank=False, null=False, primary_key=True, max_length=255)
 	DepartureDate = models.DateField(blank=True,null=True)	
 	DepartureTime = models.TimeField(blank=True, null=True)
 	ArrivalTime = models.TimeField(blank=True, null=True)
