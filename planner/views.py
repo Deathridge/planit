586d268aa0f71e2486e2bd7a454359d37cef06cc
Deathridge <danielrbetteridge@gmail.com>
2015-08-20 17:02:53 +0800

    json dumps for returned values

--- planner/views.py
+++ planner/views.py
@@ -3,6 +3,14 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
 import requests, random, string, os,sys
 from planit.settings import BASE_DIR
+from django.core import serializers
+from flights.models import Flight
 
 def planner(request):
 	return render(request, 'planner.html')
+
+def events_json(request):
+	flights = Flight.objects.all()
+	json_flights = serializers.serialize('json', flights)
+
+	return HttpResponse(json_flights)
