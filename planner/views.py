537c7a0524116f17ad1ad0b2b9d07f5a313d6f0e
Deathridge <danielrbetteridge@gmail.com>
2015-08-21 13:56:55 +0800

    list of data

--- planner/views.py
+++ planner/views.py
@@ -20,7 +20,7 @@ def planner_json(request):
 	planner_json = serializers.serialize('json', planner)		
 	
 	data = json.loads(planner_json)
-	field_data = []
+	field_data = list()
 
 	for d in data:
 		del d['pk']
