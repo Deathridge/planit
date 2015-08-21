4e8b1053da25c3d8f4216e1acf20e11cfeba9973
Deathridge <danielrbetteridge@gmail.com>
2015-08-21 12:38:53 +0800

    planner object is created then saved

--- planner/views.py
+++ planner/views.py
@@ -14,8 +14,9 @@ def planner_json(request):
 	flights = Flight.objects.all()
 	for flight in flights:
 		planner = Planner(title=flight.FlightCode + " " + flight.DepartureLocation + " " + flight.ArrivalLocation, start=flight.DepartureDate, end=flight.DepartureDate)
-		planner.save()
+		
 		planner_json = serializers.serialize('json', planner)
+		planner.save()
 		return HttpResponse(planner_json)
 
 
