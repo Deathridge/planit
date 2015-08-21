52657c77c7734cc53b85a18063a7017de1106331
Deathridge <danielrbetteridge@gmail.com>
2015-08-21 13:22:51 +0800

    update calendar to retrieve from json planner url

--- static/js/planner.js
+++ static/js/planner.js
@@ -54,7 +54,7 @@
 
         	// your event source
         	{
-            url: '/flights/json', // use the `url` property
+            url: '/planner/json', // use the `url` property
             color: 'yellow',    // an option!
             textColor: 'black'  // an option!
         	}
