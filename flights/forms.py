9964305f7cf150973377da8890f3866c4cca5c53
danielrbetteridge@gmail.com <danielrbetteridge@gmail.com>
2015-08-25 22:40:43 +0800

    background color #F0F0F0

--- flights/forms.py
+++ flights/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class SubmitFlight(forms.Form):	
-	FlightCode = forms.CharField(label='FlightCode', widget=forms.TextInput(attrs={'placeholder': 'Enter Flight code', 'class': 'form-control'}))
-	DepartureDate = forms.DateField(label='Departs',widget=forms.DateInput(attrs={'placeholder': 'Enter Departure date', 'class': 'datepicker'}))
+	FlightCode = forms.CharField(label='FlightCode', widget=forms.TextInput(attrs={'placeholder': 'QF762', 'class': 'form-control'}))
+	DepartureDate = forms.DateField(label='Departs',widget=forms.DateInput(attrs={'placeholder': 'MM/DD/YYYY', 'class': 'datepicker'}))
 	
