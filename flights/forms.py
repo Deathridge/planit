8a2dc1ed3f0eecedd538626f6bf2f358cabb6f16
Daniel Betteridge <eyes.of.daniel@gmail.com>
2015-08-30 20:08:35 +0800

    attempt to change width of flight code entry field

--- flights/forms.py
+++ flights/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 class SubmitFlight(forms.Form):	
 	FlightCode = forms.CharField(label='FlightCode', widget=forms.TextInput(attrs={'placeholder': 'QF762', 'class': 'form-control'}))
-	DepartureDate = forms.DateField(label='Departs',widget=forms.DateInput(attrs={'placeholder': 'MM/DD/YYYY', 'class': 'form-control'}))
+	DepartureDate = forms.DateField(label='Departs',widget=forms.DateInput(attrs={'placeholder': 'MM/DD/YYYY', 'class': 'datepicker'}))
 	
