f2b4a970e3e3a995d0a3e6ec2a703699e4d91d15
Deathridge <danielrbetteridge@gmail.com>
2015-08-31 10:18:42 +0800

    add touch

--- static/js/planner.js
+++ static/js/planner.js
@@ -96,7 +96,7 @@
 
     	})
 		
-		$('#calendar').addTouch();
+		$('#calendar').draggable();
 
     	viewScreenSize();
     	if (thisScreenWidth < 601) $('#calendar').fullCalendar('changeView', 'agendaDay');
