7bf35ed41e0df199d1c00c4a17a63465c2900f63
Deathridge <danielrbetteridge@gmail.com>
2015-08-31 11:54:18 +0800

    missing comma before hide

--- static/js/planner.js
+++ static/js/planner.js
@@ -89,7 +89,7 @@
         		at: 'top right',
         		// at the bottom right of...
         		target: 'mouse'
-        		} 
+        		}, 
         		hide: {
      			   event: 'unfocus'
     			}  		    
